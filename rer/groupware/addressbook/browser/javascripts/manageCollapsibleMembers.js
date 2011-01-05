88a091596b6f998204fdd21784baad04793d8270
cekk <andrea.cecchi85@gmail.com>
2011-01-05 16:36:24 +0000

    added javascripts

---
+++ rer/groupware/addressbook/browser/javascripts/manageCollapsibleMembers.js
@@ -0,0 +1,24 @@
+function setExpanded(event){
+	event.preventDefault();
+	var parent=jq(this).parent();
+	console.log(this);
+	var ul=parent.children('ul');
+	if (ul.hasClass('hiddenMembers')) {
+		ul.removeClass('hiddenMembers');
+		jq(ul).hide();
+	}
+	jq(ul).slideToggle(200);
+	jq(this).toggleClass('collapsedButton');
+	jq(this).toggleClass('expandedButton');
+}
+
+
+jq(document).ready(function() {
+	var blocks=jq('ul.roomGroupMembers');
+	var groups=jq('a.groupName');
+	jq(groups).each(function(i){
+		jq(this).addClass('collapsedButton');
+		jq(this).bind('click', setExpanded);
+	});
+});
+
